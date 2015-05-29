2240001879b56defcd1a78d5ec80a4c9c62727a7
jdy1512 <dyoung1512@gmail.com>
2015-05-29 15:02:15 +0900

    setting - 1

--- wheremasil/src/com/wheremasil/plan/controller/PlannerScheduleController.java
+++ wheremasil/src/com/wheremasil/plan/controller/PlannerScheduleController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("planner/")
+@RequestMapping("plan/")
 public class PlannerScheduleController {
 
 	@RequestMapping("info")
 	public String showInfoView() {
 		return "info.do";
 	}
+	
+	
+	
 }
